f33e1f192a4057c99c0cd92772313d0936141ae6
Mateus I <mateuspi.0398@gmail.com>
2022-02-17 18:12:22 -0300

    Exceção de Tabuleiro
    Criação da exceção de tabuleiro sem linha e coluna, posição fora do tabuleiro e duas peças na mesma posição

--- chess-system/src/boardgame/Board.java
+++ chess-system/src/boardgame/Board.java
@@ -1,21 +1,20 @@
 package boardgame;
 
 import lombok.Getter;
-import lombok.Setter;
 
 public class Board {
 	
 	@Getter
-	@Setter
 	private int rows;
 
 	@Getter
-	@Setter
 	private int columns;
 	
 	private Piece[][] pieces;
 	
 	public Board(int rows, int columns) {
+		if(rows < 1 || columns < 1)
+			throw new BoardException("Erro ao criar tabuleiro: tem que ter pelo menos 1 linha e 1 coluna!");
 		this.rows = rows;
 		this.columns = columns;
 		this.pieces = new Piece[rows][columns];
@@ -23,6 +22,8 @@ public class Board {
 	
 	public Piece piece(int row, int column)
 	{
+		if(!positionExists(row, column))
+			throw new BoardException("Posição não está dentro do tabuleiro!");
 		return pieces[row][column];
 	}
 	
@@ -33,7 +34,26 @@ public class Board {
 	
 	public void placePiece(Piece piece, Position position)
 	{
+		if(thereIsAPiece(position))
+			throw new BoardException("Já tem uma peça na posição " + position);
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
+	}
+	
+	private boolean positionExists(int row, int column)
+	{
+		return row >=0 && row < rows && column >=0 && column < columns;
+	}
+	
+	public boolean positionExists(Position position)
+	{
+		return positionExists(position.getRow(), position.getColumn());
+	}
+	
+	public boolean thereIsAPiece(Position position)
+	{
+		if(!positionExists(position))
+			throw new BoardException("Posição não está dentro do tabuleiro!");
+		return piece(position) != null;
 	}
 }
