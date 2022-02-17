0453912a59853d49d3240a6448168d3f8156ea1f
Mateus I <mateuspi.0398@gmail.com>
2022-02-17 18:33:35 -0300

    Criação de Exceção do Xadrez
    Criado uma exceção da identificação das posições do tabuleiro.

---
+++ chess-system/src/chess/ChessPosition.java
@@ -0,0 +1,33 @@
+package chess;
+
+import boardgame.Position;
+import lombok.Getter;
+
+@Getter
+public class ChessPosition {
+	private char column;
+	private int row;
+	
+	public ChessPosition(char column, int row) {
+		if(column < 'a' || column > 'h' || row < 1 || row > 8)
+			throw new ChessException("Erro ao instanciar a Posição do tabuleiro. Valido os valores de A1 até H8");
+		this.column = column;
+		this.row = row;
+	}
+	
+	protected Position toPosition()
+	{
+		return new Position(8 - row, column - 'a');
+	}
+	
+	protected static ChessPosition fromPosition(Position position)
+	{
+		return new ChessPosition((char) ('a' - position.getColumn()), 8 - position.getRow());
+	}
+
+	@Override
+	public String toString() {
+		return "" + column + row;
+	}
+	
+}
