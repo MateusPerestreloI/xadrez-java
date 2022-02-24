c6acd53afa3c06ec44d942d919665e09398c703d
Mateus I <mateuspi.0398@gmail.com>
2022-02-24 20:33:45 -0300

    Peão
    Implementação da classe peão e seus possíveis movimentos

--- chess-system/src/chess/pieces/Pawn.java
+++ chess-system/src/chess/pieces/Pawn.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 import lombok.NonNull;
@@ -22,6 +23,59 @@ public class Pawn extends ChessPiece{
 	{
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
 		
+		Position p = new Position(0, 0);
+
+		//Acima Peão Branco
+		if(getColor() == Color.WHITE)
+		{
+			//Andar 1 Casa
+			p.setValues(position.getRow() - 1, position.getColumn());
+			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) 
+				mat[p.getRow()][p.getColumn()] = true;
+			
+			//Andar 2 casas
+			p.setValues(position.getRow() - 2, position.getColumn());
+			Position p2 = new Position(position.getRow() - 1, position.getColumn());
+			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getBoard().positionExists(p2) && !getBoard().thereIsAPiece(p2) && getMoveCount() == 0)
+				mat[p.getRow()][p.getColumn()] = true;
+			
+			//Captura de peça na Diagonal esquerda
+			p.setValues(position.getRow() - 1, position.getColumn() - 1);
+			if (getBoard().positionExists(p) && isThereOpponentPiece(p)) 
+				mat[p.getRow()][p.getColumn()] = true;
+			
+			//Captura de peça na Diagonal direita
+			p.setValues(position.getRow() - 1, position.getColumn() + 1);
+			if (getBoard().positionExists(p) && isThereOpponentPiece(p)) 
+				mat[p.getRow()][p.getColumn()] = true;
+			
+		}
+		//Acima Peão Preta
+		else
+		{
+			//Andar 1 Casa
+			p.setValues(position.getRow() + 1, position.getColumn());
+			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) 
+				mat[p.getRow()][p.getColumn()] = true;
+
+			//Andar 2 casas
+			p.setValues(position.getRow() + 2, position.getColumn());
+			Position p2 = new Position(position.getRow() + 1, position.getColumn());
+			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getBoard().positionExists(p2) && !getBoard().thereIsAPiece(p2) && getMoveCount() == 0) 
+			{
+				mat[p.getRow()][p.getColumn()] = true;
+			}
+		
+			//Captura de peça na Diagonal esquerda
+			p.setValues(position.getRow() + 1, position.getColumn() - 1);
+			if (getBoard().positionExists(p) && isThereOpponentPiece(p)) 
+				mat[p.getRow()][p.getColumn()] = true;
+				
+			//Captura de peça na Diagonal direita
+			p.setValues(position.getRow() + 1, position.getColumn() + 1);
+			if (getBoard().positionExists(p) && isThereOpponentPiece(p)) 
+				mat[p.getRow()][p.getColumn()] = true;
+		}
 		return mat;
 	}
 }
