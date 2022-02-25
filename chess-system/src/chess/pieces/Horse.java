0769348c2bcd07f7e81af8e6eac327ea1b7f8ed0
Mateus I <mateuspi.0398@gmail.com>
2022-02-24 21:01:56 -0300

    Cavalo
    Implementação do Cavalo e seus possíveis movimentos

--- chess-system/src/chess/pieces/Horse.java
+++ chess-system/src/chess/pieces/Horse.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 import lombok.NonNull;
@@ -17,10 +18,59 @@ public class Horse extends ChessPiece{
 		return "H";
 	}
 	
+	public boolean canMove(Position position)
+	{
+		ChessPiece p = (ChessPiece) getBoard().piece(position);
+		
+		return p == null || p.getColor() != getColor();
+	}
+	
 	@Override
 	public boolean[][] possibleMoves()
 	{
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		
+		Position p = new Position(0, 0);
+		
+		//Cima Esquerda
+		p.setValues(position.getRow() - 2, position.getColumn() - 1);
+		if(getBoard().positionExists(p) && canMove(p))
+			mat[p.getRow()][p.getColumn()] = true;
+		
+		//Cima Direita
+		p.setValues(position.getRow() - 2, position.getColumn() + 1);
+		if(getBoard().positionExists(p) && canMove(p))
+			mat[p.getRow()][p.getColumn()] = true;
+		
+		//Baixo Esquerda
+		p.setValues(position.getRow() + 2, position.getColumn() - 1);
+		if(getBoard().positionExists(p) && canMove(p))
+			mat[p.getRow()][p.getColumn()] = true;
+		
+		//Baixo Direita
+		p.setValues(position.getRow() + 2, position.getColumn() + 1);
+		if(getBoard().positionExists(p) && canMove(p))
+			mat[p.getRow()][p.getColumn()] = true;
+		
+		//Esquerda Cima
+		p.setValues(position.getRow() - 1, position.getColumn() - 2);
+		if(getBoard().positionExists(p) && canMove(p))
+			mat[p.getRow()][p.getColumn()] = true;
+		
+		//Esquerda Baixo
+		p.setValues(position.getRow() + 1, position.getColumn() - 2);
+		if(getBoard().positionExists(p) && canMove(p))
+			mat[p.getRow()][p.getColumn()] = true;
+		
+		//Direita Cima
+		p.setValues(position.getRow() - 1, position.getColumn() + 2);
+		if(getBoard().positionExists(p) && canMove(p))
+			mat[p.getRow()][p.getColumn()] = true;
+		
+		//Direita Baixo
+		p.setValues(position.getRow() + 1, position.getColumn() + 2);
+		if(getBoard().positionExists(p) && canMove(p))
+			mat[p.getRow()][p.getColumn()] = true;
 		
 		return mat;
 	}
